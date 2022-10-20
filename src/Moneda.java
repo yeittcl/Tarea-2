5a9f0e836c74afb34c7dc13655f30999f87ea0fd
yeittcl <raulzapatavillalobos@gmail.com>
2022-10-20 20:01:01 -0300

    creacion clase moneda y sus subclases

---
+++ src/Moneda.java
@@ -0,0 +1,6 @@
+public abstract class Moneda {
+    public Moneda(){
+    }
+    public abstract String getValor();
+}
+
---
+++ src/Moneda100.java
@@ -0,0 +1,10 @@
+public class Moneda100 extends Moneda {
+    private int valor;
+    public Moneda100(){
+        super();
+        valor = 100;
+    }
+    public String getValor(){
+        return valor + "";
+    }
+}
